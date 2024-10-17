3ca765d717f7c7c0186aa7f55bcd18ef50b53d9f
Zaki Arain <zaki.dev@pm.me>
2024-10-17 09:32:41 -0500

    remove console.log

--- plugins/circleci/src/components/BuildWithStepsPage/lib/ActionOutput/ActionOutput.tsx
+++ plugins/circleci/src/components/BuildWithStepsPage/lib/ActionOutput/ActionOutput.tsx
@@ -61,7 +61,7 @@ export const ActionOutput = ({
             actionOutput.map(({ message }: { message: string }) => message),
           );
         }
-      }).catch(err => console.log(err, url));
+      }); // handle error
   }, [url]);
 
   const timeElapsed = durationHumanized(action.start_time, action.end_time);
